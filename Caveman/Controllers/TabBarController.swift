6423ad05b23dfdbbed189a0333a31509379fb06f
Jessica Trinh <jtjessicatrinh@gmail.com>
2020-03-15 04:29:19 -0700

    add home page to tab bar

--- Caveman/Controllers/TabBarController.swift
+++ Caveman/Controllers/TabBarController.swift
@@ -7,3 +7,21 @@
 //
 
 import Foundation
+import UIKit
+import SwiftUI
+
+class TabBarController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    func setupViewControllers() {
+        let homeVC = HomeViewController()
+        homeVC.tabBarItem = UITabBarItem(title: "Home", image: UIImage(named: "home"), tag: 0)
+        let homeNav = UINavigationController(rootViewController: homeVC)
+        
+//        viewControllers = [homeNav]
+    }
+}
