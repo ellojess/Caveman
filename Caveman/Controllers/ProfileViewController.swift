6ef2443dbd2abb54f3ba797c2f644a405109ce64
Jessica Trinh <jtjessicatrinh@gmail.com>
2020-03-16 00:42:21 -0700

    initiate profile page

--- Caveman/Controllers/ProfileViewController.swift
+++ Caveman/Controllers/ProfileViewController.swift
@@ -7,3 +7,22 @@
 //
 
 import Foundation
+import UIKit
+
+class ProfileViewController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setViews()
+        print("profileVC")
+    }
+    
+    override func loadView() {
+      super.loadView()
+    }
+    
+    func setViews(){
+        self.view.backgroundColor = UIColor(red:0.95, green:0.95, blue:0.95, alpha:1.0)
+    }
+    
+}
