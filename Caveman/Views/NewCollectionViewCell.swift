abe0148ec2a91ac3342c41f6ab873c41a152684f
Jessica Trinh <jtjessicatrinh@gmail.com>
2020-03-16 14:27:17 -0700

    create new order cell view

---
+++ Caveman/Views/NewCollectionViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  NewCollectionViewCell.swift
+//  Caveman
+//
+//  Created by Bo on 3/16/20.
+//  Copyright © 2020 Jessica Trinh. All rights reserved.
+//
+
+import UIKit
+
+class NewCollectionViewCell: UICollectionViewCell {
+    
+    static var identifier: String = "newOrderCell"
+    
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        self.image.contentMode = .scaleAspectFit
+    }
+    
+    public func setUp(with model: New) {
+        image.image = model.image
+        title.text = model.title
+    }
+
+}
